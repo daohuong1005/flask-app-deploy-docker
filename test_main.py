a9671b76221e0a4753cb23c6c99d17f35a131c89
daohuong1005 <16112000hp@gmail.com>
2024-10-09 00:45:00 +0700

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,40 @@
+'''
+Tests for jwt flask app.
+'''
+import os
+import json
+import pytest
+
+import main
+
+SECRET = 'TestSecret'
+TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3Mjk2MTMyMjQsIm5iZiI6MTcyODQwMzYyNCwiZW1haWwiOiJhYmNAZ21haWwuY29tIn0.jvuE7v3rv-oZEEoV4e-pD5Z_I1XXBh4K9VSVHo6aW1k'
+EMAIL = 'abc@gmail.com'
+PASSWORD = 'password'
+
+@pytest.fixture
+def client():
+    os.environ['JWT_SECRET'] = SECRET
+    main.APP.config['TESTING'] = True
+    client = main.APP.test_client()
+
+    yield client
+
+
+
+def test_health(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json == 'Healthy'
+
+
+def test_auth(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
